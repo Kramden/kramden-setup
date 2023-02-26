d0587847e9e4056d6b97c06685d0a4090d8f742f
Ken VanDine <ken.vandine@canonical.com>
2023-02-25 22:31:30 -0500

    Added some upower usage

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -1,11 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:kramden_setup/main.dart' as app;
 import 'package:yaru_widgets/yaru_widgets.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
   testWidgets('integration', (tester) async {
     await app.main();
     await tester.pumpAndSettle();
--- lib/src/widgets/footer.dart
+++ lib/src/widgets/footer.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
 
 class Footer extends StatelessWidget {
   const Footer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ButtonBar(
+    return const ButtonBar(
       alignment: MainAxisAlignment.center,
       children: [
-        Html(
-          data: "Kramden",
-          shrinkWrap: true,
-          //onAnchorTap: (url, _, __, ___) => launchUrlString(url!),
-        ),
+        Text("Kramden"),
       ],
     );
   }
