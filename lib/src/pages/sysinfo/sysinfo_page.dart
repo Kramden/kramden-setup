72c5dc4821339691c2d3628634957ac76ab74d9b
Ken VanDine <ken.vandine@canonical.com>
2023-02-26 19:35:12 -0500

    Fixed up upower usage

--- lib/src/pages/sysinfo/sysinfo_page.dart
+++ lib/src/pages/sysinfo/sysinfo_page.dart
@@ -3,29 +3,12 @@ import 'package:provider/provider.dart';
 import 'package:upower/upower.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
-
 import '../../../widgets.dart';
 
 class SysinfoPage extends StatefulWidget {
   //static var buildDetail;
 
-  const SysinfoPage({super.key});
-
-  void getBatteryInfo() async {
-    final upower = UPowerClient();
-    await upower.connect();
-    for (var device in upower.devices) {
-      if (device.type == UPowerDeviceType.battery) {
-        print("Capacity: ${device.capacity}");
-        print("Percentage: ${device.percentage}");
-        print("Icon: ${device.iconName}");
-      }
-    }
-    upower.close();
-    //final battery = await upower.devices();
-    //final battery = await upower.getBattery();
-    //print(battery);
-  }
+  SysinfoPage({super.key});
 
   static Widget buildIcon(BuildContext context) {
     return const Icon(YaruIcons.computer);
@@ -38,7 +21,7 @@ class SysinfoPage extends StatefulWidget {
   static Widget buildDetail(BuildContext context) {
     return ChangeNotifierProvider(
       create: (BuildContext context) {},
-      child: const SysinfoPage(),
+      child: SysinfoPage(),
     );
   }
 
@@ -47,21 +30,65 @@ class SysinfoPage extends StatefulWidget {
 }
 
 class _SysinfoPageState extends State<SysinfoPage> {
+  late String batteryCapacity = "";
+  late String batteryPercentage = "";
+  late String batteryIcon = "";
+
+  void getBatteryInfo() async {
+    final upower = UPowerClient();
+    await upower.connect();
+    for (var device in upower.devices) {
+      if (device.type == UPowerDeviceType.battery) {
+        print("Capacity: ${device.capacity}");
+        print("Percentage: ${device.percentage}");
+        print("Icon: ${device.iconName}");
+        batteryCapacity = device.capacity.toString();
+        batteryPercentage = device.percentage.toString();
+        batteryIcon = device.iconName.toString();
+      }
+    }
+    upower.close();
+  }
+
   @override
   void initState() {
     super.initState();
+    getBatteryInfo();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Padding(
-        padding: EdgeInsets.all(kYaruPagePadding),
-        child: Text("System Info"),
+      body: Padding(
+        padding: const EdgeInsets.all(kYaruPagePadding),
+        child: Column(
+          children: [
+            const Text("System Info"),
+            YaruSection(
+              child: Column(children: [
+                YaruTile(
+                  title: const Text("Battery Capacity"),
+                  subtitle: Text(batteryCapacity),
+                  style: YaruTileStyle.normal,
+                ),
+                YaruTile(
+                  title: const Text("Battery Percentage"),
+                  subtitle: Text(batteryPercentage),
+                  style: YaruTileStyle.normal,
+                ),
+                YaruTile(
+                  title: const Text("Battery Capacity"),
+                  subtitle: Text(batteryCapacity),
+                  style: YaruTileStyle.normal,
+                ),
+              ]),
+            ),
+          ],
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       floatingActionButton: FloatingActionButton(
-        onPressed: () => widget.getBatteryInfo(),
+        onPressed: () => getBatteryInfo(),
         child: const Icon(YaruIcons.refresh),
       ),
       bottomNavigationBar: const Footer(),
